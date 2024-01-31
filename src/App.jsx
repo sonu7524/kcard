e2cc73eeee907a483b707ea74838d724838f1bcf
sonudaryani <sonu@deepneura.com>
2024-01-31 19:51:07 +0530

    update

---
+++ src/App.jsx
@@ -0,0 +1,60 @@
+import { useState } from 'react'
+import axios from 'axios'
+import './App.css'
+
+function App() {
+  const [file, setFile] = useState(null);
+  const [flags, setFlags] = useState(null);
+
+  const handleFileChange = (e) => {
+    setFile(e.target.files[0]);
+  };
+
+  const handleSubmit = async () => {
+    try {
+      const formData = new FormData();
+      formData.append('file', file);
+
+      await axios.post('http://127.0.0.1:5000/update_data', formData, {
+        headers: {
+          'Content-Type': 'multipart/form-data',
+        },
+      });
+
+      alert('File uploaded successfully!');
+    } catch (error) {
+      alert('An error occurred while uploading the file.');
+      console.error(error);
+    }
+  };
+
+  const fetchData = async () => {
+    try {
+      const response = await axios.get('http://127.0.0.1:5000/get_flags');
+      console.log(response.data);
+      setFlags(response.data);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  return (
+    <div>
+      <h1>Upload File</h1>
+      <input type="file" accept=".json" onChange={handleFileChange} />
+      <button onClick={handleSubmit}>Submit</button>
+
+      <h1>Flags</h1>
+      {flags !== null ? (
+        <ul>
+          <li>BORROWING_TO_REVENUE_FLAG: {flags['BORROWING_TO_REVENUE_FLAG']}</li>
+          <li>ISCR_FLAG: {flags['ISCR_FLAG']}</li>
+          <li>TOTAL_REVENUE_5CR_FLAG: {flags['TOTAL_REVENUE_5CR_FLAG']}</li>
+        </ul>
+      ) : null}
+      <button onClick={fetchData}>Fetch Flags</button>
+    </div>
+  );
+}
+
+export default App
